39d655324a3fa7086a70ab39cd6cc920e8bff384
MinhSang97 <minhsangnguyen463@gmail.com>
2024-01-22 23:00:54 +0700

    ECHO

--- repository/repo_impl/user_repo_impl.go
+++ repository/repo_impl/user_repo_impl.go
@@ -1,7 +1,25 @@
 package repoimpl
 
-import "app/dbutil"
+import (
+	"app/dbutil"
+	"app/model"
+	"app/repository"
+	"context"
+)
 
 type UserRepoImpl struct {
 	sql *dbutil.Sql
+}
+
+func NewUserRepo(sql *dbutil.Sql) repository.UserRepo {
+	return &UserRepoImpl{
+		sql: sql,
+	}
+}
+
+func (u UserRepoImpl) SaveUser(context context.Context, user model.User) (model.User, error) {
+	statement := `
+		INSERT INTO users(user_id)
+
+	`
 }
